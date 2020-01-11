efb3fe3635651281ae414dba08fecd25c8163d4f
YooDonggwan <dbehdrhks@naver.com>
2020-01-11 11:43:13 +0000

    testwav

---
+++ server/apicall.py
@@ -0,0 +1,18 @@
+import librosa
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.signal import butter, lfilter, freqz
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
---
+++ server/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request
+import apicall
+import librosa
+import numpy as np
+from werkzeug import secure_filename
+from matplotlib import pyplot as plt
+from scipy.signal import butter, lfilter, freqz
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello minsu"
+
+@app.route('/pitch_shift', methods=['GET','POST'])
+def get_wav():
+
+    print('only connect')
+    print(request)
+
+    if request.form.get("method") == 'PUT':
+        # starting_point = request.form.get("start")
+        # end_point = request.form.get("end")
+        print('put connect')
+        get_key1 = request.form.get("key1")
+        get_key2 = request.form.get("key2")
+        f = request.files['file']
+        filePath = "./" + secure_filename(f.filename)
+        f.save(filePath)
+
+        # start~end 파일 잘라서 load에 넣기
+        # 화음 종류 선택할 수 있게 하기? - for문 돌려서 리스트에 넣을 수 있게
+        
+        y, sr = librosa.load(secure_filename(f.filename))
+        # y_third_orig = librosa.effects.pitch_shift(y, sr, n_steps=-4)
+        # y_t = apicall.butter_bandpass_filter(y, 200.0, 400.0, sr, order=3)
+        y_test = np.zeros(len(y))
+        y_third = librosa.effects.pitch_shift(y, sr, n_steps=get_key1)
+        y_fifth = librosa.effects.pitch_shift(y, sr, n_steps=get_key2)
+        #librosa.output.write_wav('orig.wav', (y_third_orig), sr)
+        librosa.output.write_wav('third.wav', y + y_third, sr)
+        # librosa.output.write_wav('out.wav', y, sr)
+        # librosa.output.write_wav('out.wav', y_third, sr)
+        return "success"
+
+    return "failed"
+
+
+
+
+
+if __name__ =='__main__':
+    app.run(debug = True, host = '0.0.0.0', port = 7080)
+    # app.run(debug = True, port = 4001)
+    # app.run()

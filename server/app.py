d3088c7a360a335700b4ef90d2f500206e7de5cb
YooDonggwan <dbehdrhks@naver.com>
2020-01-14 14:01:50 +0000

    done

--- server/app.py
+++ server/app.py
@@ -4,6 +4,7 @@ import numpy as np
 from werkzeug import secure_filename
 from matplotlib import pyplot as plt
 from scipy.signal import butter, lfilter, freqz
+import pyrubberband as pyrb
 
 app = Flask(__name__)
 
@@ -13,6 +14,12 @@ def download_file(filename):
     print(filename)
     return send_from_directory("/home/dbehdrhks/mindonglody/sound/", filename)
 
+@app.route('/sound2/<filename>', methods=['GET', 'POST'])
+def download_file2(filename):
+    # filename = request.args.get('filename', "third.wav")
+    print(filename)
+    return send_from_directory("/home/dbehdrhks/mindonglody/sound2/", filename)
+
 @app.route('/pitch_shift', methods=['GET','POST'])
 def get_wav():
 
@@ -20,8 +27,7 @@ def get_wav():
     print(request)
 
     if request.form.get("method") == 'PUT':
-        # starting_point = request.form.get("start")
-        # end_point = request.form.get("end")
+        
         print('put connect')
         get_key1 = request.form.get("key1")
         get_key2 = request.form.get("key2")
@@ -35,12 +41,33 @@ def get_wav():
         # 화음 종류 선택할 수 있게 하기? - for문 돌려서 리스트에 넣을 수 있게
         
         y, sr = librosa.load(f)
+        # y, sr = librosa.load('/home/dbehdrhks/mindonglody/sound/yaseng2.wav')
         # y_third_orig = librosa.effects.pitch_shift(y, sr, n_steps=-4)
         # y_t = apicall.butter_bandpass_filter(y, 200.0, 400.0, sr, order=3)
+        
         y_third = librosa.effects.pitch_shift(y, sr, n_steps=get_key1)
         y_fifth = librosa.effects.pitch_shift(y, sr, n_steps=get_key2)
+
         #librosa.output.write_cd wav('orig.wav', (y_third_orig), sr)
         librosa.output.write_wav('../sound/' + f.filename, y + y_third, sr)
+        librosa.output.write_wav('../sound2/' + f.filename, y + y_third + y_fifth*0.7, sr)
+
+        # infiles = [y, y_third]
+        # outfile = '../sound/' + f.filename
+
+        # data = []
+
+        # for infile in infiles:
+        #     w = wave.open(infile, 'rb')
+        #     data.append( [w.getparams(), w.readframes(w.getnframes())])
+        #     w.close()
+
+        # output = wave.open(outfile, 'wb')
+        # output.setparams(data[0][0])
+
+        # output.writeframes(data[0][1])
+        # output.writeframes(data[1][1])
+        # output.close()
         # librosa.output.write_wav('out.wav', y, sr)
         # librosa.output.write_wav('out.wav', y_third, sr)
         return "success"
--- server/pitch_shift.py
+++ server/pitch_shift.py
@@ -4,6 +4,8 @@
 # import wave
 # import numpy as np
 # from scipy.io import wavfile
+# import pyrubberband as pyrb
+
 
 # #http://people.csail.mit.edu/hubert/pyaudio/
 # #special thanks to http://zulko.github.io/blog/2014/03/29/soundstretching-and-pitch-shifting-in-python/
@@ -187,6 +189,8 @@ import librosa
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.signal import butter, lfilter, freqz
+import pyrubberband as pyrb
+
 
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
@@ -204,13 +208,14 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
 
 
 
-y, sr = librosa.load('yaseng2.wav')
+y, sr = librosa.load('do.wav')
 #y_third_orig = librosa.effects.pitch_shift(y, sr, n_steps=-4)
-y_t = butter_bandpass_filter(y, 200.0, 400.0, sr, order=3)
-y_test = np.zeros(len(y))
-y_third = librosa.effects.pitch_shift(y, sr, n_steps=4)
+y_stretch = pyrb.time_stretch(y, sr, 2.0)
+# y_t = butter_bandpass_filter(y, 200.0, 400.0, sr, order=3)
+# y_test = np.zeros(len(y))
+y_third = librosa.effects.pitch_shift(y_stretch, sr, n_steps=4)
 #y_fifth = librosa.effects.pitch_shift(y, sr, n_steps=7)
 #librosa.output.write_wav('orig.wav', (y_third_orig), sr)
-librosa.output.write_wav('third.wav', y_third + y_test, sr)
+librosa.output.write_wav('third.wav', y_third, sr)
 # librosa.output.write_wav('out.wav', y, sr)
 # librosa.output.write_wav('out.wav', y_third, sr)
